a26fa6e48a387d962c68fb7c1704d8c9e845672e
zacktmiller92 <zacktmiller92@gmail.com>
2021-08-14 10:56:01 -0400

    moved copy and write file functions to a module called generate-site

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const inquirer = require('inquirer');
-const fs = require('fs');
+// const fs = require('fs');
 const generatePage = require('./src/page-template');
+const { writeFile, copyFile } = require('./utils/generate-site.js');
+
+
 
 const promptUser = () => {
   return inquirer.prompt([
@@ -132,11 +135,18 @@ Add a New Project
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    const pageHTML = generatePage(portfolioData);
-
-    fs.writeFile('./index.html', pageHTML, err => {
-      if (err) throw err;
-
-      console.log('Portfolio complete! Check out index.html to see the output!');
-    });
+    return generatePage(portfolioData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
   });
